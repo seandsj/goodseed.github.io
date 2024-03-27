d64d4c244e70c2cc05d18ba8dde7a2a616f95b80
sean.e.desanjose <sean.e.desanjose@gmail.com>
2024-03-27 15:32:39 +0800

    update

---
+++ assets/js/dropdown.js
@@ -0,0 +1,8 @@
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
